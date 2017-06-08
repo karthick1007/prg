87a2af0369d58aa326ac6a3cab832f80eb6c215a
karthick1007 <karthickroshan6@gmail.com>
2017-06-08 10:16:20 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,18 @@
+import java.io.*; 
+import java.util.*;
+public class vowel
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+char c=s.next().charAt(0);
+if(((c>='a')&&(c<='z'))||((c>='A')&&(c<='Z')))
+{
+System.out.println("alphabet");
+}
+else
+{
+System.out.println("not");
+}
+}
+}
